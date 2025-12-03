e040d8d7afce3727b9b764be2a196da4b3818ddf
Prohack101010 <28umutmehmet28@gmail.com>
2025-12-03 15:21:24 +0000

    well

--- setup/unix.sh
+++ setup/unix.sh
@@ -14,6 +14,6 @@ haxelib install hscript 2.4.0 --quiet
 haxelib install tjson 1.4.0 --quiet #for fucking mods system (haxe.Json brokes the modlist)
 haxelib install hxCodec --quiet #That clearly my repo
 haxelib git hxcpp https://github.com/beihu235/hxcpp --quiet #Just a normal hxcpp
-haxelib git lime https://github.com/HomuHomu833-haxelibs/lime int-delta --quiet #repo owner helped to fix termux problem
+haxelib git lime https://github.com/HomuHomu833-haxelibs/lime --quiet #repo owner helped to fix termux problem
 haxelib install openfl 9.3.3 --quiet #Most stable version of openfl
 echo Finished!
