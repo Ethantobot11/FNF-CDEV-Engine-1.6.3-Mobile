dd45376dc6de031c5c7ed960148ed398af98432b
ArkoseLabsOfficial <28umutmehmet28@gmail.com>
2026-03-19 06:54:54 +0000

    here we go again

--- setup/unix.sh
+++ setup/unix.sh
@@ -9,11 +9,11 @@ echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
 haxelib git flixel https://github.com/PsychExtendedThings/flixel 5.6.1 --quiet
 haxelib install flixel-addons 3.2.2 --quiet
-haxelib install flixel-ui 2.4.0 --quiet #I don't know why is it exist
+haxelib install flixel-ui 2.4.0 --quiet
 haxelib install hscript 2.4.0 --quiet
-haxelib install tjson 1.4.0 --quiet #for fucking mods system (haxe.Json brokes the modlist)
-haxelib install hxCodec --quiet #That clearly my repo
-haxelib git hxcpp https://github.com/beihu235/hxcpp --quiet #Just a normal hxcpp
-haxelib git lime https://github.com/HomuHomu833-haxelibs/lime --quiet #repo owner helped to fix termux problem
-haxelib install openfl 9.3.3 --quiet #Most stable version of openfl
+haxelib install tjson 1.4.0 --quiet
+haxelib install hxCodec --quiet
+haxelib git hxcpp https://github.com/beihu235/hxcpp --quiet
+haxelib git lime https://github.com/ArkoseLabsOfficial/lime --quiet
+haxelib install openfl 9.3.3
 echo Finished!
